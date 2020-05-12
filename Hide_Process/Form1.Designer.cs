06050da31e0f5a80d4b7d27e7b94c656eb67e74a
wanchuan <854616408@qq.com>
2020-05-13 04:06:03 +0800

    .

---
+++ Hide_Process/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Hide_Process
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.processListView = new System.Windows.Forms.ListView();
+            this.btnHide = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // processListView
+            // 
+            this.processListView.HideSelection = false;
+            this.processListView.Location = new System.Drawing.Point(3, 4);
+            this.processListView.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.processListView.Name = "processListView";
+            this.processListView.Size = new System.Drawing.Size(422, 416);
+            this.processListView.TabIndex = 0;
+            this.processListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnHide
+            // 
+            this.btnHide.Location = new System.Drawing.Point(431, 4);
+            this.btnHide.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnHide.Name = "btnHide";
+            this.btnHide.Size = new System.Drawing.Size(121, 206);
+            this.btnHide.TabIndex = 1;
+            this.btnHide.Text = "隐藏";
+            this.btnHide.UseVisualStyleBackColor = true;
+            this.btnHide.Click += new System.EventHandler(this.btnHide_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(431, 214);
+            this.btnShow.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(121, 206);
+            this.btnShow.TabIndex = 2;
+            this.btnShow.Text = "显示";
+            this.btnShow.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 426);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnHide);
+            this.Controls.Add(this.processListView);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView processListView;
+        private System.Windows.Forms.Button btnHide;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
---
+++ Hide_Process/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices; // DllImport
+using System.Diagnostics;
+using System.Collections;
+
+namespace Hide_Process{
+    public partial class Form1 : Form{
+        public delegate bool EnumWindowCallback(int hwnd, int lParam);//得到进程的主窗口句柄
+        [DllImport("user32")]
+        public static extern int ShowWindow(int hwnd, int nCmdShow); //指定窗口状态
+        ArrayList handleArray = null; //保存隐藏的进程
+
+        public Form1(){
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e){  //初始化ListView,存取当前进程
+            processListView.View = View.List;
+            processListView.CheckBoxes = true;
+            handleArray = new ArrayList();
+            loadProcessList();
+            this.btnHide.Click += FuncHide;
+            this.btnShow.Click += FuncShow;
+        }
+
+        private void loadProcessList(){
+            Process[] processVar = Process.GetProcesses();
+            processListView.Items.Clear(); //防进程重复
+            handleArray.Clear();
+
+            foreach (Process process in processVar){     //循环获取进程加入ListView中
+                if (!String.IsNullOrEmpty(process.MainWindowTitle)){ //获取标题
+                    IntPtr mainHandle = process.MainWindowHandle;
+                    handleArray.Add(mainHandle.ToInt32());
+                    processListView.Items.Add(process.MainWindowTitle, handleArray.Count-1);
+                }
+            }
+        }
+
+        private void FuncHide(object sender, EventArgs e){         //指定窗口状态-隐藏
+            for (int i = 0; i < processListView.Items.Count; i++){
+                if (processListView.Items[i].Checked == true) {
+                    ShowWindow((int)handleArray[i], 0);
+                }
+            }
+        }
+
+        private void FuncShow(object sender, EventArgs e){         //指定窗口状态-显示
+            for (int i = 0; i < processListView.Items.Count; i++){
+                if (processListView.Items[i].Checked == true){
+                    ShowWindow((int)handleArray[i], 5);
+                }
+            }
+        }
+
+        private void btnHide_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
